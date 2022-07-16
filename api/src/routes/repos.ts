8068f673d5a7b870e6223151f7e56d8fb71f0240
Hannah Silver <hannah_silver@hotmail.com>
2022-07-16 16:46:13 -0400

    fixed server api fetch

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,6 +1,6 @@
 import { Router, Request, Response } from 'express';
-import data from '../../data/repos.json';
-
+import repoData from '../../data/repos.json';
+const axios = require('axios');
 export const repos = Router();
 
 repos.get('/', async (_: Request, res: Response) => {
@@ -10,6 +10,21 @@ repos.get('/', async (_: Request, res: Response) => {
 
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
 
-  const filteredData = data.filter((data) => data.fork === false);
-  res.json(filteredData);
+  try {
+    const response = await axios.get(
+      'https://api.github.com/users/silverorange/repos'
+    );
+
+    // console.log(response.data);
+
+    //aggregating the 2 endpoints
+    let data = repoData.concat(response.data);
+
+    //filtering false forks
+    const filteredData = data.filter((data) => data.fork === false);
+
+    res.json(filteredData);
+  } catch (err) {
+    console.log(err);
+  }
 });
