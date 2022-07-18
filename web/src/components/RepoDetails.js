eb61149d8bbbc026afc43cab17194304b3290bef
Hannah Silver <hannah_silver@hotmail.com>
2022-07-18 15:19:42 -0400

    fetch repo id with params

--- web/src/components/RepoDetails.js
+++ web/src/components/RepoDetails.js
@@ -1,6 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 export function RepoDetails() {
+  const repoId = useParams();
+  const [repo, setRepo] = useState({});
+
+  console.log(repo);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/repos')
+      .then((res) => res.json())
+      .then((data) => {
+        // const repos = data;
+        const response = data.filter((r) => r.id === Number(repoId.id));
+        // console.log(response);
+        setRepo(response[0]);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [repoId]);
+
   return (
     <div>
       <p>hi</p>
